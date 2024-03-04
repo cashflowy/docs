15be0c30b57f673d8262f9529f05a6db2a246146
Fahimansari <fahimpjan@gmail.com>
2024-03-04 13:22:04 +0530

    Adding Hot jar tracking

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
-import React from 'react'
-import { Redirect } from '@docusaurus/router'
+import { Redirect } from '@docusaurus/router';
+import React from 'react';
+
+import Hotjar from '@hotjar/browser';
+
+const siteId = 3890572;
+const hotjarVersion = 6;
+
+Hotjar.init(siteId, hotjarVersion);
 
 export default function Home() {
   return <Redirect to="/overview/why-cashflowy" />
